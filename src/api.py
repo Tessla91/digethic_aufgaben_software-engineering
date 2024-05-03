0ec4558080086498bf43bbc7d3cbefb42f7e54cf
Tessa <160845611+Tessla91@users.noreply.github.com>
2024-05-03 17:28:28 +0200

    ufgabe 2 done

--- src/api.py
+++ src/api.py
@@ -7,6 +7,19 @@ import pickle
 app = Flask(__name__)
 CORS(app)
 
+training_data = pd.read_csv(os.path.join("data/auto-mpg-training-data.csv"))
+
+print(training_data)
 @app.route("/", methods=["GET"])
 def index():
-    return{"hello, world"}
+    return {"message": "hello, world"}
+
+@app.route("/hello_world", methods=["GET"])
+def hello_world():
+    return ("<p>Hello World!<p>")
+
+@app.route("/training_data", methods=["GET"])
+def get_training_data():
+    return Response(training_data.to_json(), mimetype="application/json")
+
+
