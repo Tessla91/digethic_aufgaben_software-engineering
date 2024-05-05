2a7a8597b27ed82032957fcbee39d90af6f0f35c
Tessa <160845611+Tessla91@users.noreply.github.com>
2024-05-05 15:10:11 +0200

    Aufgabe 3 done

--- src/api.py
+++ src/api.py
@@ -9,7 +9,12 @@ CORS(app)
 
 training_data = pd.read_csv(os.path.join("data/auto-mpg-training-data.csv"))
 
-print(training_data)
+# Modell laden
+file_to_open = open(os.path.join("data/models/regression.pickle"), "rb")
+trained_model = pickle.load(file_to_open)
+file_to_open.close ()
+
+# Ausgaben in Browser mit wsgi erzeugen
 @app.route("/", methods=["GET"])
 def index():
     return {"message": "hello, world"}
@@ -21,5 +26,19 @@ def hello_world():
 @app.route("/training_data", methods=["GET"])
 def get_training_data():
     return Response(training_data.to_json(), mimetype="application/json")
+
+@app.route("/predict", methods=["GET"])
+def predict():
+    zylinder = float(request.args.get("zylinder"))
+    ps = float(request.args.get("ps"))
+    gewicht = float(request.args.get("gewicht"))
+    beschleunigung = float(request.args.get("beschleunigung"))
+    baujahr = float(request.args.get("baujahr"))
+
+    if (zylinder and ps and gewicht and beschleunigung and baujahr):
+        prediction = trained_model.predict([[zylinder, ps, gewicht, beschleunigung, baujahr]])
+        return {"result": prediction [0]}
+    else:
+        return Response ("Please provide all necessary parameters to ger a prediciton", mimetype="application/json")
 
 
